e93d7505b8f8368ea01dc932daa1499e93bbd5e6
黄海 <hh474440@cainiao.com>
2025-06-09 15:57:30 +0800

    Update

--- src/main/java/com/user/center/UserCenterApplication.java
+++ src/main/java/com/user/center/UserCenterApplication.java
@@ -3,11 +3,13 @@ package com.user.center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableCaching // or you can add this annotation in configuration class
 @EnableRetry
+@EnableDiscoveryClient
 public class UserCenterApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/user/center/dto/req/CreateUserDetailReqVO.java
+++ src/main/java/com/user/center/dto/req/CreateUserDetailReqVO.java
@@ -1,7 +1,6 @@
 package com.user.center.dto.req;
 
 import com.user.center.util.validation.AllowedValues;
-import com.user.center.util.validation.DayAfter;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -34,7 +33,6 @@ public class CreateUserDetailReqVO {
      * Note: when LocalDateTime serialize into JSON, will be something like this:
      * "day":{"dayOfMonth":22,"dayOfWeek":"THURSDAY","dayOfYear":203,"hour":13,"minute":25,"month":"JULY","monthValue":7,"nano":920000000,"second":35,"year":2021,"chronology":{"id":"ISO","calendarType":"iso8601"}}
      */
-    @DayAfter(message = "Day is not valid", dayAfter = 4)
     private String day;
 
 }
