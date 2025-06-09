6058005b35ca3e6582c4b13847dfde8ff8ad68d8
黄海 <hh474440@cainiao.com>
2025-06-09 22:26:06 +0800

    Update

--- src/main/java/com/user/center/UserCenterApplication.java
+++ src/main/java/com/user/center/UserCenterApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching // or you can add this annotation in configuration class
@@ -14,6 +17,12 @@ public class UserCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
