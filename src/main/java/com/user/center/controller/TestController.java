e59e28fb5452f074366e5951940eb46ef4f7951d
黄海 <hh474440@cainiao.com>
2025-06-11 18:03:08 +0800

    Update

--- src/main/java/com/user/center/controller/TestController.java
+++ src/main/java/com/user/center/controller/TestController.java
@@ -25,6 +25,13 @@ public class TestController {
         return orderCenterRemoteClient.getDefaultEchoMsg();
     }
 
+    /**
+     * Spring Cloud Alibaba Sentinel：https://github.com/alibaba/spring-cloud-alibaba/wiki/Sentinel
+     * 在生产环境中使用 Sentinel：https://github.com/alibaba/Sentinel/wiki/%E5%9C%A8%E7%94%9F%E4%BA%A7%E7%8E%AF%E5%A2%83%E4%B8%AD%E4%BD%BF%E7%94%A8-Sentinel
+     *
+     * @param name
+     * @return
+     */
     @GetMapping(value = "/hello/{name}")
     public String apiHello(@PathVariable String name) {
         return testService.sayHello(name);
